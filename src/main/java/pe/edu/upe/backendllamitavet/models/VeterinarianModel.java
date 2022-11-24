1e08729c204803a624e3ab63494dc50dbada606c
MaxTer43 <u201416643@upc.edu.pe>
2022-11-24 10:47:38 -0500

    Added array for reviews

--- src/main/java/pe/edu/upe/backendllamitavet/models/VeterinarianModel.java
+++ src/main/java/pe/edu/upe/backendllamitavet/models/VeterinarianModel.java
@@ -14,6 +14,7 @@ public class VeterinarianModel {
     private String location;
     private Integer phone;
     private String img_url;
+    private static String[] review;
 
     public Long getId() {
         return id;
@@ -53,5 +54,13 @@ public class VeterinarianModel {
 
     public void setPhone(Integer phone) {
         this.phone = phone;
+    }
+
+    public String[] getReview() {
+        return review;
+    }
+
+    public void setReview(String[] review) {
+        this.review = review;
     }
 }
